cd6c15aafa8b9ef098f496daff807353be429ae7
Adebayo Osunbiyi <adebayo.osunbiyi.a@gmail.com>
2023-01-18 17:20:44 +0100

    Socket client added

---
+++ Client.py
@@ -0,0 +1,23 @@
+import socket
+
+HEADER = 64
+PORT = 9600
+SERVER = "172.16.2.194"
+ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+
+send("Hello From The Other Side")
--- Server.py
+++ Server.py
@@ -18,17 +18,19 @@ def handle_client(conn, addr):
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
-        msg_length = int(msg_length)
-        msg = conn.recv(msg_length).decode(FORMAT)
-        if msg == DISCONNECT_MESSAGE:
-            connected = False
-        print(f"[{addr}] {msg}")
-        
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            print(f"[{addr}] {msg}")
+
     conn.close()
 
 
 def start():
     server.listen()
+    print(f"[LISTENING] Server is listerning on {SERVER}")
     while True:
         conn, addr = server.accept()
         thread = threading.Thread(target=handle_client(conn, addr))
